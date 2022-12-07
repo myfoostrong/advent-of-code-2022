bc1bdd65a1252c10f07c54c5e06343d5420108c5
Conor <conor@foo.solutions>
2022-12-06 23:06:37 -0500

    Adding code from day6 solution

--- 6/day6.go
+++ 6/day6.go
@@ -25,36 +25,49 @@ func part2() int {
 }
 
 func main() {
-	f, err := os.Open("./test.txt")
-	// f, err := os.Open("./day6_input.txt")
+	// f, err := os.Open("./test.txt")
+	f, err := os.Open("./day6_input.txt")
 	if err != nil {
 		// handle error
 	}
 	scanner := bufio.NewScanner(f)
 
-	part1_marker := 0
-	part2_marker := ""
+	marker := 0
 	curr := ""
+	char_count := 14 //4 for Part 1
 	
 	for scanner.Scan() {
 		row := scanner.Text()
-		signal := [26]int{}
-		curr = row[:4]
+		// signal := [26]int{}
 		print(curr,"\n")
-		for i, char := range row {
-			if (i > 3) {
-				c := int(char) - int('a')
-				signal[c] += 1
-				if signal[c] != 1 {
-					
-				} else {
-					print(i,"\n")
+		for i := range row {
+			match := false
+			if (i > char_count - 1) {
+				signal := row[i-char_count:i]
+				// print("Checking signal ",signal,"\n")
+				for j,v := range signal {
+					for k:=j+1; k<char_count; k++ {
+						// fmt.Printf("Does %s match %s?\n",signal[k],v)
+						if (string(signal[k]) == string(v)) {
+							// print("Yes\n")
+							match = true
+							break
+						}
+					}
+					if (match) {
+						break
+					}
+				}
+				if (!match) {
+					marker = i
 					break
 				}
 			}
 		}
+		// fmt.Printf("Part1:\n%d\n",marker)
+		fmt.Printf("Part2:\n%d\n",marker)
 	}
 	
-	fmt.Printf("Part1:\n%s\n",part1_marker)
-	fmt.Printf("Part2:\n%s\n",part2_marker)
+	
+	
 }
