efd503d65691d4cafe7dfe26b5962c7ca823cf3e
trik <myfoostrong@gmail.com>
2024-08-19 23:51:40 -0400

    Solution1

--- 14/day14.go
+++ 14/day14.go
@@ -15,7 +15,7 @@ func Solve1() {
 		log.Fatal(err)
 	}
 	var answer int
-	var xMax, yMax int
+	var xMin, yMin, xMax, yMax int = 500, 500, 0, 0
 	var rocks [][][]int
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
@@ -35,9 +35,15 @@ func Solve1() {
 					if val > xMax {
 						xMax = val
 					}
+					if val < xMin {
+						xMin = val
+					}
 				} else if i == 1 {
 					if val > yMax {
 						yMax = val
+					}
+					if val < yMin {
+						yMin = val
 					}
 				}
 			}
@@ -45,8 +51,97 @@ func Solve1() {
 		}
 		rocks = append(rocks, points)
 	}
-	answer = xMax + yMax
-	fmt.Println("Day13 Solution 1: ", answer)
+	xMax += 4
+	xMin -= 1
+	yMax += 2
+	yMin -= 1
+	grid := make([][]bool, yMax)
+	for i := range yMax {
+		grid[i] = make([]bool, xMax)
+	}
+	for _, row := range rocks {
+		var last []int
+		for i, point := range row {
+			if i == 0 {
+				last = point
+				grid[point[1]][point[0]] = true
+				continue
+			}
+
+			if last[0] == point[0] {
+				dir := point[1] - last[1]
+				if dir < 0 {
+					for j := last[1]; j > point[1]-1; j-- {
+						grid[j][point[0]] = true
+					}
+				} else if dir == 0 {
+					log.Fatal("Shouldn't be zero")
+				} else {
+					for j := last[1]; j < point[1]+1; j++ {
+						grid[j][point[0]] = true
+					}
+				}
+			} else if last[1] == point[1] {
+				dir := point[0] - last[0]
+				if dir < 0 {
+					for j := last[0]; j > point[0]-1; j-- {
+						grid[point[1]][j] = true
+					}
+				} else if dir == 0 {
+					log.Fatal("Shouldn't be zero")
+				} else {
+					for j := last[0]; j < point[0]+1; j++ {
+						grid[point[1]][j] = true
+					}
+				}
+			}
+			last = point
+		}
+	}
+	printGrid(grid, xMin, xMax, 0, yMax)
+	var count, drop int
+sand:
+	x := 500
+	for y := range yMax {
+		if grid[y][x] {
+			if x > 0 && grid[y][x-1] {
+				if grid[y][x+1] {
+					grid[y-1][x] = true
+					// printGrid(grid, xMin, xMax, 0, 55)
+					count += 1
+					goto sand
+				} else {
+					x += 1
+				}
+			} else {
+				x -= 1
+			}
+		}
+	}
+	drop += 1
+	if drop < 2 {
+		goto sand
+	}
+	answer = count
+	printGrid(grid, xMin, xMax, 0, yMax)
+	fmt.Println("Day14 Solution 1: ", answer)
+}
+
+func printGrid(grid [][]bool, xMin, xMax, yMin, yMax int) {
+	fmt.Println("\n")
+	for i := yMin; i < yMax; i++ {
+		for j, c := range grid[i][xMin:xMax] {
+			a := "."
+			if c {
+				a = "#"
+			}
+			if i == 0 && j == 500-xMin {
+				a = "x"
+			}
+			fmt.Print(a)
+		}
+		fmt.Print("\n")
+	}
 }
 
 func Solve2() {
